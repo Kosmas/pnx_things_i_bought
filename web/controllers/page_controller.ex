d4161037f6406654e898c64c42b0def6cf019bde
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-08-27 18:46:58 +0200

    added index method in controller, changed index template and changed title

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule PnxThingsIBought.PageController do
   use Phoenix.Controller
 
   def index(conn, _params) do
-    render conn, "index"
+    purchases = PnxThingsIBought.Queries.purchases_query
+    render conn, "index", purchases: purchases
   end
 end
