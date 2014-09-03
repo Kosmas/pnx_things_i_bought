241f53a1f3f1c95e5781e2112f97f64e1c869dcb
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-03 17:30:27 +0200

    added new and save and also added alias for the Router

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,8 +1,21 @@
 defmodule PnxThingsIBought.PageController do
   use Phoenix.Controller
+  alias PnxThingsIBought.Router
 
   def index(conn, _params) do
     purchases = PnxThingsIBought.Queries.purchases_query
     render conn, "index", purchases: purchases
+  end
+
+  def new(conn, _params) do
+    render conn, "new"
+  end
+
+  def save(conn, params) do
+    # is there a better way to convert the param["cost"] to float?
+    {cost, _} = Float.parse(params["cost"])
+    purchase = %PnxThingsIBought.Purchases{name: params["name"], cost: cost}
+    PnxThingsIBought.Repo.insert(purchase)
+    redirect conn, Router.index_path(:index)
   end
 end
