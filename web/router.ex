ee1f47978315a3fd16f9c277ac20c80bbec3e70c
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-03 17:29:08 +0200

    changed named route from page_path to index_path

--- web/router.ex
+++ web/router.ex
@@ -2,5 +2,7 @@ defmodule PnxThingsIBought.Router do
   use Phoenix.Router
 
   plug Plug.Static, at: "/static", from: :pnx_things_i_bought
-  get "/", PnxThingsIBought.PageController, :index, as: :page
+  get "/", PnxThingsIBought.PageController, :index, as: :index
+  get "/new", PnxThingsIBought.PageController, :new
+  post "/new", PnxThingsIBought.PageController, :save, as: :save
 end
